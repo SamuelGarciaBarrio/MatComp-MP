106a31c6edf5f9ea37a8c69cf29a147bf634cb2a
Samug <samuel.garciabarrio@edu.uah.es>
2025-02-09 13:26:07 +0100

    Empieza el trabajo

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/Circle.java
@@ -0,0 +1,42 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class Circle {
+    private double radius;
+
+    // Constructors (overloaded)
+    /** Constructs a Circle instance with default value for radius and color */
+    public Circle() { // 1st (default) constructor
+        radius = 1.0;
+    }
+
+    /** Constructs a Circle instance with the given radius and default color */
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    /** Returns the radius */
+    public double getRadius() {
+        return radius;
+    }
+
+    /** Sets the radius to the given value */
+    public void setRadius(double radius) {
+        this.radius = radius; // "this.radius" refers to the instance variable
+        // "radius" refers to the method's argument
+    }
+
+    /** Returns the area of this Circle instance */
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+
+    /** Returns el perimeter of this Circle instance */
+    public double getCircumference() {
+        return 2*radius*Math.PI;
+    }
+
+    public String toString() {
+        return "Circle[radius=" + radius + " color=" + "]";
+    }
+
+}

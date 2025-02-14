f6a7c9ec813843d760fa02a8c549804c807d9725
Samug <samuel.garciabarrio@edu.uah.es>
2025-02-14 11:56:32 +0100

    Empaqueto los ejercicios

--- Test/java/es/uah/matcomp/mp/e1/ejerciciosclases/DateTest.java
+++ Test/java/es/uah/matcomp/mp/e1/ejerciciosclases/DateTest.java
@@ -1,5 +1,6 @@
 package es.uah.matcomp.mp.e1.ejerciciosclases;
 
+import es.uah.matcomp.mp.e1.ejerciciosclases.Date.Date;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;

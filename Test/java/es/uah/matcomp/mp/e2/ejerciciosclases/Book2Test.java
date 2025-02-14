10eed7e5de6a3586277e15de30b9897be5df389e
Samug <samuel.garciabarrio@edu.uah.es>
2025-02-14 19:52:21 +0100

    Ejercicio 2.2, cambio Book2Test de paquete

--- Test/java/es/uah/matcomp/mp/e2/ejerciciosclases/Book2Test.java
+++ Test/java/es/uah/matcomp/mp/e2/ejerciciosclases/Book2Test.java
@@ -1,6 +1,7 @@
-package es.uah.matcomp.mp.e2.ejerciciosclases.Book;
+package es.uah.matcomp.mp.e2.ejerciciosclases;
 
 import es.uah.matcomp.mp.e2.ejerciciosclases.Author.Author;
+import es.uah.matcomp.mp.e2.ejerciciosclases.Book.Book2;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
